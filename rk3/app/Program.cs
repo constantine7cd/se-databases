517ec7d7eba4fc3e36ed596f75111b16abaddb90
Konstantin <thunderball7cd@yandex.ru>
2018-12-22 01:50:41 +0300

    rk3 added

---
+++ rk3/app/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.first();
+            //p.second();
+            //p.third();
+            p.task3();
+        }
+
+        void third()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["try"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+
+                command.CommandText = String.Format("select DATEDIFF(SECOND, U.time_, U2.time_) as diff, " +
+         "E.FIO from Uchet as U join Uchet " +
+         "as U2 On U.type_ = 1 and U2.type_ = 2 and U.id_empl = U2.id_empl and " +
+         "U.sysdate = U2.sysdate join Empl as E On U.id_empl = E.ID " +
+         "where DAY(U.sysdate) = DAY(GETDATE()) and " +
+          "MONTH(U.sysdate) = MONTH(GETDATE()) and " +
+          "YEAR(U.sysdate) = YEAR(GETDATE()) " +
+         "ORDER BY(DATEDIFF(HOUR, U.time_, U2.time_))");
+                command.Connection = connection;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                DataTable dt = ds.Tables[0];
+                Console.WriteLine("Работники с наименьшим количеством времени работы:");
+                int min = 0;
+                foreach (DataRow row in dt.Rows)
+                {
+                    // получаем все ячейки строки
+                    var cells = row.ItemArray;
+
+                    if (min == 0)
+                    {
+                        min = (int)cells[0];
+                    }
+                    else if ((int)cells[0] != min)
+                    {
+                        break;
+                    }
+
+                    Console.Write("\t{0} \t{1} секунд", cells[1], cells[0]);
+
+                    Console.WriteLine();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Что то сломалось..." + ex.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+                connection.Close();
+            }
+        }
+
+        void second()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["try"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+
+                command.CommandText = String.Format("select DEP.Title from Department as DEP where  2 < ( "+
+                    "SELECT COUNT(*) from Uchet as U join "+
+                    "Empl as E ON E.ID = U.id_empl "+
+                    "where sysdate > CAST('2018-12-10' AS Date) and "+
+                    "time_ > CAST('9:00:00' AS time) and E.Depart = DEP.ID and "+
+                    "U.type_ = 1)");
+                command.Connection = connection;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                DataTable dt = ds.Tables[0];
+                Console.WriteLine("Найденные отделы:");
+                foreach (DataRow row in dt.Rows)
+                {
+                    // получаем все ячейки строки
+                    var cells = row.ItemArray;
+                    foreach (object cell in cells)
+                        Console.Write("\t{0}", cell);
+                    Console.WriteLine();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Что то сломалось..." + ex.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+                connection.Close();
+            }
+        }
+        
+        void first()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["try"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                
+                command.CommandText = String.Format("select distinct E.FIO, D.Title from Uchet as U" +
+                                                                                   " join Empl as E ON E.ID = U.id_empl" +
+                                                                                   " join Department as D ON D.ID = E.Depart" +
+                                                                                   " where U.time_ > CAST('9:00:00' AS time) and" +
+                                " U.type_ = 1");
+                command.Connection = connection;
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("Вот что удалось найти:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["FIO"]);
+                    Console.WriteLine(reader["Title"]);
+                }
+                
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Что то сломалось..." + ex.Message);
+                
+            }
+            finally
+            {
+                Console.ReadLine();
+                connection.Close();
+            }
+        }
+
+        public void task3()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["try"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+
+                SqlCommand command1 = new SqlCommand("select * from on_work()", connection);
+                SqlDataReader reader = command1.ExecuteReader();
+                Console.WriteLine("Вот что удалось найти:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["FIO"]);
+                }
+
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Что то сломалось..." + ex.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ rk3/task2/task2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Reflection;
+
+namespace task2
+{
+    [Table(Name = "Department")]
+    public class Department
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column(Name = "Title")]
+        public string Title { get; set; }
+        [Column(Name = "Date_create")]
+        public DateTime Date_create { get; set; }
+    }
+
+    [Table(Name = "Empl")]
+    public class Empl
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column(Name = "FIO")]
+        public string FIO { get; set; }
+        [Column(Name = "Date_create")]
+        public DateTime Date_create { get; set; }
+        [Column(Name = "Depart")]
+        public int Depart { get; set; }
+    }
+
+    [Table(Name = "Uchet")]
+    public class Uchet
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column(Name = "id_empl")]
+        public int id_empl { get; set; }
+        [Column(Name = "sysdate")]
+        public DateTime sysdate { get; set; }
+        [Column(Name = "day_week")]
+        public string day_week { get; set; }
+        [Column(Name = "time_")]
+        public DateTime time_ { get; set; }
+        [Column(Name = "type_")]
+        public int type_ { get; set; }
+    }
+
+    
+
+    class Program
+    {
+        static void GetLateEmpl()
+        {
+            string connectionString = @"Data Source=DESKTOP-3N6D323\SQLEXPRESS;Initial Catalog=RK_exam;Integrated Security=True";
+            DataContext db = new DataContext(connectionString);
+            TimeSpan ts = TimeSpan.Parse("9:00");
+            var query = from uchet_ in db.GetTable<Uchet>()
+                        where (uchet_.time_.TimeOfDay > ts) && (uchet_.type_ == 1)
+                        join empl in db.GetTable<Empl>() on uchet_.id_empl equals empl.Id
+                        join dep in db.GetTable<Department>() on empl.Depart equals dep.Id
+                        select new { FIO_ = empl.FIO, Dep = dep.Title };
+            query = query.Distinct();
+            foreach (var em in query)
+            {
+                Console.WriteLine("{0,15} \t{1,15}", em.FIO_, em.Dep);
+            }
+            Console.WriteLine("End");
+            Console.Read();
+        }
+
+        static void GetLateEmplTenDays()
+        {
+            string connectionString = @"Data Source=DESKTOP-3N6D323\SQLEXPRESS;Initial Catalog=RK_exam;Integrated Security=True";
+            DataContext db = new DataContext(connectionString);
+            TimeSpan ts = TimeSpan.Parse("9:00");
+            DateTime ts_date = new DateTime(2018, 12, 11, 18, 30, 25);
+            
+            var query = from dep in db.GetTable<Department>()
+                select new {id = dep.Id, tit = dep.Title};
+                        
+            foreach (var em in query)
+            {
+                var we = from uchet_ in db.GetTable<Uchet>()
+                         join empl in db.GetTable<Empl>() on uchet_.id_empl equals empl.Id
+                         where (uchet_.time_.TimeOfDay > ts) && (uchet_.type_ == 1) && (uchet_.sysdate > ts_date.Date) && empl.Depart == em.id
+                         select new { id = em.id, tit = em.tit };
+                if (we.Count() > 2)
+                    Console.WriteLine("{0,15}\n", em.tit);
+            }
+            Console.WriteLine("End");
+            Console.Read();
+        }
+
+        static void GetMinWork()
+        {
+            string connectionString = @"Data Source=DESKTOP-3N6D323\SQLEXPRESS;Initial Catalog=RK_exam;Integrated Security=True";
+            DataContext db = new DataContext(connectionString);
+            TimeSpan ts = TimeSpan.Parse("9:00");
+            DateTime ts_date = DateTime.Today;
+
+            var query = from uchet_ in db.GetTable<Uchet>()
+                     join empl in db.GetTable<Empl>() on uchet_.id_empl equals empl.Id
+                     join uchet_2 in db.GetTable<Uchet>() on uchet_.id_empl equals uchet_2.id_empl
+                        where uchet_2.type_ == 2 && uchet_.type_ == 1 && uchet_.sysdate.Date == ts_date.Date &&
+                        uchet_.sysdate.Date == uchet_2.sysdate.Date
+                        //orderby uchet_2.time_.TimeOfDay - uchet_.time_.TimeOfDay
+                        select new { id = empl.Id, name = empl.FIO, time = uchet_2.time_.TimeOfDay - uchet_.time_.TimeOfDay};
+
+            //query = query.Distinct();
+            TimeSpan tspp = query.First().time;
+            foreach (var em in query)
+            {
+                if (tspp != em.time)
+                    return;
+                Console.WriteLine("{0,15} {1,15}\n", em.name, em.time);
+            }
+            
+            Console.WriteLine("End");
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            //GetLateEmpl();
+            //GetLateEmplTenDays();
+            GetMinWork();
+        }
+    }
+}
